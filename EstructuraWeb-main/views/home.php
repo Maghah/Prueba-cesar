a863f8932ed3b50a86c352422438f0e81a47d0d8
Maghah <86864893+Maghah@users.noreply.github.com>
2022-05-23 14:39:06 -0800

    Add files via upload

---
+++ EstructuraWeb-main/views/home.php
@@ -0,0 +1,4 @@
+<h1>Home</h1>
+uwuwuw
+
+<script type="text/javascript" src="./scripts/home.js?v1.0.0"></script>
---
+++ EstructuraWeb-main/views/navbar.php
@@ -0,0 +1,117 @@
+<link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+<script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
+<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+
+
+<!------ Include the above in your HEAD tag ---------->
+
+</div><!DOCTYPE html>
+<html>
+    <head>
+        <title> MAGHA OPTICA </title>
+
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" >
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css">
+        <link rel="stylesheet" href="../res/css/navbar.css">
+        <link rel="stylesheet" href="css/animate.css">
+        <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
+        <link href="../res/css/navbar.css" rel="stylesheet" type="text/css">
+    </head>
+    <body>
+
+    <header>
+<!-- ======================================Navigation Bar================================================= -->
+        <nav class="navbar navbar-expand-lg navStyle">
+            <a class="brand-navbar" href="#"><img src="../res/imagen/vision.png" alt="Responsive image" height="60px"></a>
+            <button class="navbar-toggler" data-toggle="collapse" data-target="#mainMenu">
+                <span><i class="fas fa-align-right iconStyle"></i></span>
+            </button>
+            <div class="collapse navbar-collapse" id="mainMenu">
+                <ul class="navbar-nav ml-auto navList">
+
+                    <li class="nav-item active"><a href="home.php" class="nav-link"><i class="fas fa-home"></i>HOME<span class="sr-only">(current)</span></a>
+                    </li>
+
+                    <li class="dropdown" class="nav-item active"><a class="dropdown-toggle" data-toggle="dropdown" href="#">Servivios <span  class="fas fa-cogs"></span></a>
+              <ul class="dropdown-menu">
+                  <li><a href="login.php">Tipos de armazon</a></li>
+                  <li><a href="login.php">Lentes graduadas</a></li>
+              </ul>
+
+              <li class="dropdown" class="nav-item active"><a class="dropdown-toggle" data-toggle="dropdown" href="#">Novedades <span  class="fas fa-briefcase"></span></a>
+              <ul class="dropdown-menu">
+              <li><a href="https://www3.animeflv.net/">Lentes de contacto</a></li>
+                  <li><a href="login.php">Marcas</a></li>
+              </ul>
+
+              <li class="dropdown" class="nav-item active"><a class="dropdown-toggle" data-toggle="dropdown" href="#">Citas <span  class="fas fa-phone"></span></a>
+              <ul class="dropdown-menu">
+              <li><a href="#">Registro</a></li>
+              </ul>
+
+              <li class="dropdown" class="nav-item active"><a class="dropdown-toggle" data-toggle="dropdown" href="#">Acerca <span  class="fas fa-users"></span></a>
+              <ul class="dropdown-menu">
+                  <li><a href="#">Servicios</a></li>
+                  <li><a href="#">Información</a></li>
+              </ul>
+
+            </div>
+        </nav>
+        
+
+<!-- ======================================carousel================================================= -->
+        
+        <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+                <ol class="carousel-indicators">
+                  <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+                  <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+                  <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+                </ol>
+                <div class="carousel-inner">
+                  <div class="carousel-item active">
+                    <img class="d-block w-100" src="image/slide1.jpg" role="listbox">
+
+                        <div class="carousel-caption">
+                                <h1 class="animated fadeInDown">Welcome</h1>
+                                <p class="animated fadeInUp">Aliquam quis unde nobis aspernatur,<br/> perferendis sed explicabo obcaecati provident repudiandae.</p>
+                                <button class="btn btn-defualt animated zoomIn">Read More</button>
+                        </div>
+                  </div>
+                  <div class="carousel-item">
+                    <img class="d-block w-100" src="image/slide2.jpeg" alt="..." role="listbox">
+                    <div class="carousel-caption">
+                            <h1 class="animated fadeInDown">Welcome</h1>
+                            <p class="animated fadeInUp">Aliquam quis unde nobis aspernatur,<br/> perferendis sed explicabo obcaecati provident repudiandae.</p>
+                            <button class="btn btn-defualt animated zoomIn">Read More</button>
+                    </div>
+                  </div>
+                  <div class="carousel-item">
+                    <img class="d-block w-100" src="image/slide3.jpg" alt="..." role="listbox">
+                    <div class="carousel-caption">
+                            <h1 class="animated zoomIn">We Design We Develop</h1>
+                            <p class="animated zoomIn">Aliquam quis unde nobis aspernatur,<br/> perferendis sed explicabo obcaecati provident repudiandae.</p>
+                            <button class="btn btn-defualt animated zoomIn">Read More</button>
+                    </div>
+                  </div>
+                </div>
+                <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+                  <span class="carousel-control" aria-hidden="true"><i class="fas fa-long-arrow-alt-left"></i></span>
+                  <span class="sr-only">Previous</span>
+                </a>
+                <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+                  <span class="carousel-control" aria-hidden="true"><i class="fas fa-long-arrow-alt-right"></i></span>
+                  <span class="sr-only">Next</span>
+                </a>
+              </div>
+              
+    </header>
+
+    </body>
+
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" ></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+</html>
+
+
+<script src="./scripts/navbar.js?v1.0.0"></script>
